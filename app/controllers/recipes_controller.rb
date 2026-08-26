a268a1fb9064737ddfb46b0555b32d3346c6839f
cd ~/code/$GITHUB_USERNAME/dotfiles && zsh git_setup.sh <ps.ringdal@outlook.com>
2026-08-27 00:12:04 +0100

    Implement core recipe controller functionality

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,2 +1,19 @@
-class RecipeController < ApplicationController
+class RecipesController < ApplicationController
+  def index
+    @recipes = Recipe.all
+  end
+
+  def create
+    @recipe = Recipe.new
+    @recipe.save
+  end
+
+  def show
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def destroy
+    @recipe = Recipe.find(params[:id])
+    @recipe.destroy
+  end
 end
