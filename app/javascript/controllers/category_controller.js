1835b74f27f70bc99328013f599ff7b511bece28
cd ~/code/$GITHUB_USERNAME/dotfiles && zsh git_setup.sh <ps.ringdal@outlook.com>
2026-09-11 22:42:50 +0100

    refactor: clean up navbar markup and fix stimulus controller scoping

--- app/javascript/controllers/category_controller.js
+++ app/javascript/controllers/category_controller.js
@@ -9,7 +9,7 @@ const mealPlanCard = (meal) => {
   <a href="/recipes/${meal.idMeal}" class="recipe-link">
     <div>
       <div class="card mb-2">
-        <i class="fa-solid fa-plus toggle-icon" data-action="click->category#selectMeal" data-meal-id="${meal.idMeal}"   data-meal-plan-review-target="addIcon"></i>
+        <i class="fa-solid fa-plus toggle-icon" data-action="click->category#selectMeal" data-meal-id="${meal.idMeal}" data-meal-plan-review-target="addIcon"></i>
         <img src="${meal.strMealThumb}">
       </div>
       <div class="card-body">
@@ -68,11 +68,11 @@ export default class extends Controller {
       this.selectedCountTarget.innerHTML = count -= 1
     }
 
-    if (count > 0) {
-      this.selectedMealsTarget.classList.remove("d-none")
-    } else {
-      this.selectedMealsTarget.classList.add("d-none")
-    }
+    // if (count > 0) {
+    //   this.selectedMealsTarget.classList.remove("d-none")
+    // } else {
+    //   this.selectedMealsTarget.classList.add("d-none")
+    // }
   }
 
 }
